94958fa6a8b125981c115e966cb05088071d5cb0
Joao Gonzalez <gonza807@purdue.edu>
2024-12-11 12:20:49 -0500

    The new Debug Flag ECE565CA

---
+++ GEM5_ECE56500/src/debug/ece565ca-debug.cc
@@ -0,0 +1,8 @@
+#include "debug/ece565ca-debug.hh"
+
+namespace gem5::debug {
+
+SimpleFlag ECE565CA("ECE565CA", "Debug flag for ECE565 Project Custom Analysis");
+
+} // namespace gem5::debug
+
---
+++ GEM5_ECE56500/src/debug/ece565ca-debug.hh
@@ -0,0 +1,13 @@
+#ifndef __DEBUG_ECE565CA_HH__
+#define __DEBUG_ECE565CA_HH__
+
+#include "base/debug.hh"
+
+namespace gem5::debug {
+
+extern SimpleFlag ECE565CA;
+
+} // namespace gem5::debug
+
+#endif // __DEBUG_ECE565CA_HH__
+
