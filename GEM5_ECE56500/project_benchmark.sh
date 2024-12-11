82838abdf07cef426844fd2bdf7b7df231a91f05
Joao Gonzalez <gonza807@purdue.edu>
2024-12-10 20:09:42 -0500

    Added the support for the debug flag and correct the minor CPU selection

--- GEM5_ECE56500/project_benchmark.sh
+++ GEM5_ECE56500/project_benchmark.sh
@@ -4,6 +4,7 @@ set -Eo pipefail
 
 BASEDIR="$(dirname `readlink -e $0`)"
 OUTPUTDIR="$BASEDIR/Project_Benchmarks"
+OPT="${1:-opt}"
 
 echo "==========================================================="
 echo " PROCESSING PROJECT BENCHMARK"
@@ -19,7 +20,10 @@ CURROUTDIR="$OUTPUTDIR/`date +%Y-%m-%d`"
 for BENCH in "${BENCHMARKS[@]}"
 do	echo " Running benchmark '${BENCH^^}' .-." 
 	echo "---------------------------------------"
-	$BASEDIR/build/ARM/gem5.opt --outdir="$CURROUTDIR/$BENCH" $BASEDIR/configs/spec/spec_se.py -b "$BENCH" --maxinsts=10000000 --bp-type=TournamentBP --l1d_size=64kB --l1i_size=16kB --caches --l2cache
+	if [ $OPT = "debug" ]
+	then	$BASEDIR/build/ARM/gem5.${OPT} --outdir="$CURROUTDIR/$BENCH" --debug-flags=MinorMem,MinorCPU --debug-file=$CURROUTDIR/debug.log $BASEDIR/configs/spec/spec_se.py -b "$BENCH" --maxinsts=10000000 --bp-type=TournamentBP --l1d_size=64kB --l1i_size=16kB --caches --l2cache --cpu-type=MinorCPU 
+	else	$BASEDIR/build/ARM/gem5.${OPT} --outdir="$CURROUTDIR/$BENCH" $BASEDIR/configs/spec/spec_se.py -b "$BENCH" --maxinsts=10000000 --bp-type=TournamentBP --l1d_size=64kB --l1i_size=16kB --caches --l2cache --cpu-type=MinorCPU
+	fi
 	echo "---------------------------------------"
 	echo
 done
