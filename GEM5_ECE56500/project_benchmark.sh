3d6974182b0dddc55e8a11683b87947fef6d9dab
Joao Gonzalez <gonza807@purdue.edu>
2024-12-11 12:19:44 -0500

    Final project simulation file

--- GEM5_ECE56500/project_benchmark.sh
+++ GEM5_ECE56500/project_benchmark.sh
@@ -4,6 +4,7 @@ set -Eo pipefail
 
 BASEDIR="$(dirname `readlink -e $0`)"
 OUTPUTDIR="$BASEDIR/Project_Benchmarks"
+LCT_LVL=${1:-2}; shift
 OPT="${1:-opt}"
 
 echo "==========================================================="
@@ -21,8 +22,8 @@ for BENCH in "${BENCHMARKS[@]}"
 do	echo " Running benchmark '${BENCH^^}' .-." 
 	echo "---------------------------------------"
 	if [ $OPT = "debug" ]
-	then	$BASEDIR/build/ARM/gem5.${OPT} --outdir="$CURROUTDIR/$BENCH" --debug-flags=MinorMem,MinorCPU --debug-file=$CURROUTDIR/debug.log $BASEDIR/configs/spec/spec_se.py -b "$BENCH" --maxinsts=10000000 --bp-type=TournamentBP --l1d_size=64kB --l1i_size=16kB --caches --l2cache --cpu-type=MinorCPU 
-	else	$BASEDIR/build/ARM/gem5.${OPT} --outdir="$CURROUTDIR/$BENCH" $BASEDIR/configs/spec/spec_se.py -b "$BENCH" --maxinsts=10000000 --bp-type=TournamentBP --l1d_size=64kB --l1i_size=16kB --caches --l2cache --cpu-type=MinorCPU
+	then	$BASEDIR/build/ARM/gem5.${OPT} --outdir="$CURROUTDIR/$BENCH" --debug-flags=ECE565CA --debug-file=$CURROUTDIR/debug-${BENCH,,}.log $BASEDIR/configs/spec/spec_se.py -b "$BENCH" --maxinsts=10000000 --bp-type=TournamentBP --l1d_size=64kB --l1i_size=16kB --caches --l2cache --cpu-type=MinorCPU --lct-confidence-lvl=${LCT_LVL} 
+	else	$BASEDIR/build/ARM/gem5.${OPT} --outdir="$CURROUTDIR/$BENCH" $BASEDIR/configs/spec/spec_se.py -b "$BENCH" --maxinsts=10000000 --bp-type=TournamentBP --l1d_size=64kB --l1i_size=16kB --caches --l2cache --cpu-type=MinorCPU --lct-confidence-lvl=${LCT_LVL}
 	fi
 	echo "---------------------------------------"
 	echo
